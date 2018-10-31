6864d8ab5a7054dcc10adc99ee53ac4db1c392b9
Caprara, Steve <gub8cap@ent.rt.csaa.com>
2018-10-31 10:02:19 -0700

    added comments to explain usage and reasoning.

--- src/main/java/stringParser.java
+++ src/main/java/stringParser.java
@@ -13,13 +13,17 @@ public class stringParser
                 return "";
             }
 
+            //I like using stack for this as it's LIFO
             Stack stack = new Stack();
+            //Creating Array of Strings delimited by space.
             String[] strArray = str.split(" ");
 
             for (String s : strArray) {
+                //Push each Array element in the Array onto the Stack.
                 stack.push(s);
             }
 
+            //Using StringBuilder for it's ability to append Strings
             StringBuilder sb = new StringBuilder();
             while (!stack.empty()) {
                 sb.append(stack.pop()).append(" ");
